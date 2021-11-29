927ab7f930d27d8a91d344dfab36b03d9a0eab9e
melhamli <91897259+melhamli@users.noreply.github.com>
2021-11-29 19:49:55 +0100

    doc à modifier pour connecter à firebase
    voir pdf "documentation" pour instructions

---
+++ environment.ts
@@ -0,0 +1,21 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCErLr4tTl98cv-r6P1n2PB8mXmQdWhvoE",
+    authDomain: "ingeson-final.firebaseapp.com",
+    databaseURL: "https://console.firebase.google.com/u/0/project/ingeson-final/firestore/data/~2F",
+    projectId: "ingeson-final",
+    storageBucket: "ingeson-final.appspot.com",
+    messagingSenderId: "941063582747",
+    appId: "1:941063582747:web:e727290bd4006b11ddf4be",
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
